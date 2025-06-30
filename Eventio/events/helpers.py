4f10b4fe76079d978c6c0a0e90dc93d3be156558
Marek Stefaniak <madgotten@wp.pl>
2025-06-30 16:33:10 +0200

    Add argument to specify size of an event banner

--- Eventio/events/helpers.py
+++ Eventio/events/helpers.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageOps
 from django.core.files.base import ContentFile
 
 
-def validate_image(image):
+def validate_image(image, size=(500, 300)):
     img = Image.open(image)
     img.verify()
     img = Image.open(image)
@@ -11,7 +11,7 @@ def validate_image(image):
     if img.mode in ("RGBA", "LA", "P"):
         img = img.convert("RGB")
 
-    img = ImageOps.contain(img, (500, 300))
+    img = ImageOps.contain(img, size)
     temp_img = BytesIO()
     img.save(temp_img, format="WEBP", optimize=True, quality=95)
     temp_img.seek(0)
