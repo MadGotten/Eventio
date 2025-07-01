c2d16630f1db03ba04a5e8f9d2ca113ef9659cd4
Marek Stefaniak <madgotten@wp.pl>
2025-07-02 00:05:36 +0200

    Add event list caching for dynamic template content and add caching invalidation based on event model signals

--- Eventio/events/signals.py
+++ Eventio/events/signals.py
@@ -1,5 +1,7 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
+from django.core.cache.utils import make_template_fragment_key
 from django.dispatch import receiver
+from django.core.cache import cache
 from .models.events import Event
 
 
@@ -8,3 +10,10 @@ def delete_cascade_event_img(sender, instance, **kwargs):
     """Cleanup associated image in bucket after event deletion"""
     if instance.banner:
         instance.banner.delete(save=False)
+
+
+@receiver([post_delete, post_save], sender=Event)
+def invalidate_event_cache(sender, instance, **kwargs):
+    """Invalidate event cache after event deletion or creation"""
+    key = make_template_fragment_key("event_list")
+    cache.delete(key)
