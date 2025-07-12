9b448fdd7fbef9112c4b7bbda02fcbf92256e43f
Marek Stefaniak <madgotten@wp.pl>
2025-07-13 00:56:56 +0200

    Change review delete to use HTTP Delete method with htmx

--- Eventio/events/views/review.py
+++ Eventio/events/views/review.py
@@ -1,36 +1,38 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.views.decorators.http import require_POST
 from events.models import Event, Review
 from events.forms import ReviewForm
+from django_htmx.http import HttpResponseClientRefresh
 
 
-@require_POST
 @login_required
 def review_create(request, pk):
     event = get_object_or_404(Event, id=pk)
     review_form = ReviewForm(request.POST)
 
-    if review_form.is_valid():
-        review = review_form.save(commit=False)
-        review.user = request.user
-        review.event = event
-        review.save()
-        messages.success(request, "Review was submitted.")
-        return render(request, "partials/_review.html", {"review": review}, status=201)
+    if request.method == "POST":
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.event = event
+            review.save()
+            messages.success(request, "Review was submitted.")
+            return render(request, "partials/_review.html", {"review": review}, status=201)
 
-    return redirect("event_detail", pk=pk)
+    return render(
+        request, "partials/_review_form.html", {"review_form": review_form, "event": event}
+    )
 
 
 @login_required
 def review_delete(request, pk):
     review = get_object_or_404(Review, pk=pk, user=request.user)
 
-    if request.method == "POST":
+    if request.method == "DELETE":
         review.delete()
         messages.success(request, "Review was deleted.")
 
-        return redirect("event_detail", pk=review.event.id)
+        return HttpResponseClientRefresh()
 
     return render(request, "modals/delete_modal.html", {"object": review})
